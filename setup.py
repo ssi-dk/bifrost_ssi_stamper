af08793fa6c95172e306a01c9cace4d4cb207e38
Martín Basterrechea <martinbaste@gmail.com>
2021-02-12 13:30:55 +0100

    Bump version: v2_2_5 → v2_2_6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrost_ssi_stamper',
-    version='v2_2_5',
+    version='v2_2_6',
     description='Datahandling functions for bifrost (later to be API interface)',
     url='https://github.com/ssi-dk/bifrost_ssi_stamper',
     author="Kim Ng, Martin Basterrechea",
