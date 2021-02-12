f72e94a20a5ba0ae3930c4c46716142bdc5cc4ee
Martín Basterrechea <martinbaste@gmail.com>
2021-02-12 13:30:51 +0100

    bump bifrostlib

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email="kimn@ssi.dk",
     packages=find_packages(),
     install_requires=[
-        'bifrostlib >= 2.1.6',
+        'bifrostlib >= 2.1.7',
     ],
     package_data={"bifrost_ssi_stamper": ['config.yaml']},
     include_package_data=True
